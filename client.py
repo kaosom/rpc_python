3640eed8b52aef82e526423465ac6b8ce626cd8d
= <=>
2023-10-31 17:00:50 -0600

    Cambio el codigo para implementar formula cuadratica

--- client.py
+++ client.py
@@ -1,30 +1,36 @@
 '''
-        Script cliente para calcular el perímetro y área de un cuadrado mediante RPC.
+    ANGEL GABRIEL SOSA MORALES
+    SISTEMAS OPERATIVOS I
+    RPC - Cliente para Calcular Raíz Cuadrada
 
-        Este script se utiliza para conectarse a un servidor RPC que proporciona
-        dos métodos remotos, 'calc_perimetro' y 'calc_area', los cuales calculan
-        el perímetro y el área de un cuadrado, respectivamente.
+    Este script de cliente se utiliza para conectarse a un servidor RPC y calcular las raíces 
+    de una ecuación cuadrática
+    proporcionando los coeficientes a, b y c como argumentos de línea de comandos.
 
-        Args:
-            Se espera que se proporcione un argumento en la línea de comandos que
-            represente el lado del cuadrado en forma de un número entero.
+    Uso:
+    python client.py 'a: int' 'b: int' 'c: int'
 
-        Ejemplo de uso:
-            Para calcular el perímetro y el área de un cuadrado con lado 5:
-            $ python client.py 5
-            Perimetro: 20
-            Area: 25
+    Donde 'a', 'b' y 'c' son los coeficientes de la ecuación cuadrática
+    de la forma ax^2 + bx + c = 0.
+
+    Ejemplo:
+    python client.py 1 2 -3
+
+    Este ejemplo calculará las raíces de la ecuación x^2 + 2x - 3 = 0 y mostrará los resultados.
+
+    Requiere que el servidor RPC esté en funcionamiento y accesible en la dirección proporcionada.
 '''
 
 from xmlrpc.client import ServerProxy
 import sys
 
-s = ServerProxy('http://192.168.0.192:5000', allow_none=True)
+server = ServerProxy('http://192.168.0.192:5050', allow_none=True)
 arguments = sys.argv
 
 if __name__ == '__main__':
     if len(arguments) < 2:
-        print("El formato debe ser python client.py 'lado: int'")
+        print("El formato debe ser python client.py 'a: int' 'b: int' 'c: int'")
     else:
         value = int(arguments[1])
-        print(f'Perimetro: {s.calc_perimetro(value)}\nArea: {s.calc_area(value)}\n')
+        result = server.raiz(int(arguments[1]), int(arguments[2]), int(arguments[3]))
+        print(f'<-- Resultados -->\n{result}')
--- server.py
+++ server.py
@@ -1,83 +1,71 @@
 '''
     ANGEL GABRIEL SOSA MORALES 
     SISTEMAS OPERATIVOS I 
-    RPC - Calcular Area y Perimetro
-
+    RPC - Calcular raiz cuadrada
 '''
 
 from xmlrpc.server import SimpleXMLRPCServer
+from math import sqrt
+
 class RPC:
     '''
-        Clase que implementa un servidor RPC simple para cálculo de área y perímetro.
-
-        Esta clase utiliza la biblioteca SimpleXMLRPCServer
-        para registrar métodos remotos que calculan el perímetro y el área de un cuadrado.
-
-        Args:
-            direccion (str): La dirección IP en la que se va a ejecutar el servidor.
-            port (int): El puerto en el que se va a escuchar para las solicitudes RPC.
-
-        Attributes:
-            methods (list): Lista de nombres de métodos remotos disponibles en el servidor.
-
-        Methods:
-            calc_perimetro(a): Método remoto que calcula el perímetro de un cuadrado.
-            calc_area(a): Método remoto que calcula el área de un cuadrado.
-        '''
-    methods = ['calc_perimetro', 'calc_area']
+        Clase que almacena nuestro servidor simple usando SimpleXMLRPCServer
+    '''
     def __init__(self, direccion, port):
+        '''
+        Constructor de la clase RPC.
+
+        Parameters:
+            direccion (str): La dirección en la que se va a iniciar el servidor.
+            port (int): El número de puerto en el que se va a iniciar el servidor.
+        '''
         self.server = SimpleXMLRPCServer((direccion, port), allow_none=True)
-        for method in self.methods:
-            self.server.register_function(getattr(self, method))
-    def calc_perimetro(self, a):
+        self.server.register_function(getattr(self, 'raiz'))
+
+    def is_real(self, a : int, b: int, c: int) -> bool:
         '''
-        Calcula el perímetro de un cuadrado.
+        Comprueba si las raíces son números reales.
 
-        Este método toma un valor numérico 'a' que representa el lado del cuadrado
-        y calcula el perímetro multiplicando el lado por 4.
-
-        Args:
-            a (float): El lado del cuadrado.
+        Parameters:
+            a (int): Coeficiente cuadrático.
+            b (int): Coeficiente lineal.
+            c (int): Término independiente.
 
         Returns:
-            float: El valor del perímetro del cuadrado.
-
-        Ejemplo de uso:
-            >>> rpc = RPC('', 5000)
-            >>> rpc.calc_perimetro(5)
-            20.0
+            bool: True si las raíces son números reales, False en caso contrario.
         '''
-        return 4 * a
+        return ((b * b) - 4 * a * c) > 0
 
-    def calc_area(self, a):
+    def raiz(self, a: int, b: int, c: int) -> str:
         '''
-        Calcula el área de un cuadrado.
+        Calcula las raíces de una ecuación cuadrática y devuelve el resultado como una cadena.
 
-        Este método toma un valor numérico 'a' que representa el lado del cuadrado
-        y calcula el área multiplicando el lado por sí mismo (elevado al cuadrado).
-
-        Args:
-            a (float): El lado del cuadrado.
+        Parameters:
+            a (int): Coeficiente cuadrático.
+            b (int): Coeficiente lineal.
+            c (int): Término independiente.
 
         Returns:
-            float: El valor del área del cuadrado.
-
-        Ejemplo de uso:
-            >>> rpc = RPC('', 5000)
-            >>> rpc.calc_area(5)
-            25.0
+            str: Una cadena que representa las raíces de la ecuación cuadrática.
         '''
-        return a * a
-
+        if self.is_real(a, b, c):
+            root1 = ((-1 * b) + sqrt((b * b) - 4 * a * c)) / (2 * a)
+            root2 = ((-1 * b) - sqrt((b * b) - 4 * a * c)) / (2 * a)
+            return f'x1 = {root1:.2f}\nx2 = {root2:.2f}'
+        else:
+            temp = ((b * b) - (4 * a * c)) * -1
+            real_part = (-1 * b) / (2 * a)
+            imaginary_part = (sqrt(temp)) / (2 * a)
+            return f'x1 = {real_part} + {imaginary_part}i\nx2 = {real_part} - {imaginary_part}i'
 
     def run(self):
         '''
-            run(): Inicia el servidor RPC y lo pone en modo de espera para recibir solicitudes.
+        Inicia el servidor RPC y lo pone en modo de espera para recibir solicitudes.
         '''
         self.server.serve_forever()
         print("Server iniciado")
+
 if __name__ == '__main__':
-    rpc = RPC('', 5000)
+    rpc = RPC('', 5050)
     print('Iniciando servidor...')
     rpc.run()
-    
